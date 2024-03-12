af55c772f773941673a408560588a5c0b97b5c03
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-03-12 09:58:16 -0400

    Add project files.

---
+++ CarInsurences/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurences
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
